dfca069c2a81723e43ae78f9a656ba8a1c4316ca
Abubakar Bunza Mubarak <mubarakabubakar227@gmail.com>
2020-12-09 01:49:21 +0000

    Added alphabet_position()

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 
-alphabets = {chr(n): n-96 for n in range(97,123)}
+def alphabet_position(text):
+    alphabets = {chr(n): n-96 for n in range(97,123)}
+    new_string = []
+    text = text.lower()
+    for i in text:
+        if i in alphabets.keys():
+            new_string.append(alphabets.get(i))
+        else:
+            return ''
+    string_position = ' '.join(map(str,new_string))
+    return str(string_position)
 
-sentence = 'abcAdz'.lower()
-new_sentence = []
-for i in sentence:
-  if i in alphabets.keys():
-    new_sentence.append(alphabets.get(i))
-
-new = ''.join(map(str,new_sentence))
-print(new)
+print(alphabet_position('abAc'))
+# output '1 2 1 3'
